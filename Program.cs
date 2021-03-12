a764067dea7762de9e21c5753cef53bcf84be005
Barry Shovlin <bshovlin32@gmail.com>
2021-03-11 23:52:04 -0600

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -10,18 +10,22 @@ namespace Heist
 
             Team HeistTeam = new Team();
 
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine();
+
             bool planning = true;
 
             while (planning)
             {
-                Console.WriteLine("Plan your heist!");
-                Console.WriteLine();
-
+                if (HeistTeam.Members.Count >= 1)
+                {
+                    Console.WriteLine("Enter 'done' if your team is complete");
+                }
                 Console.Write("Enter a team member's name: ");
                 string name = Console.ReadLine();
-                if (name == "")
+                if (name == "done")
                 {
-                    break;
+                    planning = false;
                 }
                 else
                 {
@@ -38,20 +42,49 @@ namespace Heist
                     Member member = new Member(name, skill, courage);
                     HeistTeam.AddMember(member);
 
-                    Console.WriteLine(name);
-                    Console.WriteLine(MemberSkill);
-                    Console.WriteLine(courageFactor);
+                    // Console.WriteLine(name);
+                    // Console.WriteLine(MemberSkill);
+                    // Console.WriteLine(courageFactor);
 
-                    Console.WriteLine(HeistTeam.Members.Count);
 
-                    foreach (Member m in HeistTeam.Members)
-                    {
-                        Console.WriteLine($"{m.Name} Skill level: {m.Skill}  courage level: {m.Courage}");
-                    }
+                    // Console.WriteLine("----------------------------");
+                    // Console.WriteLine("");
+                    // Console.WriteLine("Your Team Roster");
+
+                    // Console.WriteLine($"Total team members: {HeistTeam.Members.Count}");
+
+                    // foreach (Member m in HeistTeam.Members)
+                    // {
+                    //     Console.WriteLine("----------------------------");
+                    //     Console.WriteLine($"{m.Name}   ");
+                    //     Console.WriteLine($"Skill level: {m.Skill}");
+                    //     Console.WriteLine($"Courage level: {m.Courage}");
+                    //     Console.WriteLine("");
+                    // }
                 }
+            }
+            int BankDiffLevel = 100;
+
+            int teamSkill = 0;
+
+            foreach (Member m in HeistTeam.Members)
+            {
+                teamSkill = teamSkill + m.Skill;
+            }
+
+            if (teamSkill >= BankDiffLevel)
+            {
+                Console.WriteLine("Sucess, you done did it!");
+            }
+            else
+            {
+                Console.WriteLine("You in jail");
             }
 
 
         }
     }
 }
+
+
+
